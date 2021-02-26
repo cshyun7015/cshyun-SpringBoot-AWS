a588f4fc8586207d312c8e77515f21924e185681
cshyun7015 <cshyun7015@gmail.com>
2021-02-26 23:55:18 +0900

    5th Commit

--- src/main/java/com/cshyun/book/springboot/web/IndexController.java
+++ src/main/java/com/cshyun/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.cshyun.book.springboot.web;
 
+import com.cshyun.book.springboot.config.auth.LoginUser;
+import com.cshyun.book.springboot.config.auth.dto.SessionUser;
 import com.cshyun.book.springboot.service.posts.PostsService;
 import com.cshyun.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +19,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("username", user.getName());
+        }
         return "index";
     }
 
